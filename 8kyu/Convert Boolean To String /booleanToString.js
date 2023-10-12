5416b547a5a897d9d9ae5985350f7b2da5bb79a0
monicakrystal <131429507+monicakrystal@users.noreply.github.com>
2023-10-12 10:07:24 -0700

    booleanToString.js

---
+++ 8kyu/Convert Boolean To String /booleanToString.js
@@ -0,0 +1,16 @@
+
+/*
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+
+//Answer: 
+
+function booleanToString(b){
+  return b.toString();
+}
+

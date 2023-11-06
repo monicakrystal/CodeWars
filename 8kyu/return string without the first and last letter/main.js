625839e86077d925d1d80dd560ac17ed945442a2
monicakrystal <131429507+monicakrystal@users.noreply.github.com>
2023-11-05 21:53:29 -0800

    Create main.js

---
+++ 8kyu/return string without the first and last letter/main.js
@@ -0,0 +1,16 @@
+/*
+
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. 
+You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+*/
+
+// Answer
+
+function removeChar(str) {
+  if (str.length < 2) {
+    return "String is too short to remove first and last characters";
+  }
+
+  return str.slice(1, -1);
+}
